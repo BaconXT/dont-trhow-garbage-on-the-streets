1779fc41d0cb84501af03b7b617056506fde3624
BaconXT <93720121+BaconXT@users.noreply.github.com>
2021-12-04 13:13:54 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 700);
+
+var ball_options= {
+	isStatic: false,
+	restitution:0.3,
+	friction:0,
+	density:1.2
+}
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ball = Matter.Bodies.circle(50,600,10,ball_options);
+	World.add(world,ball);
+	
+
+	groundObj = new Ground(width/2,670,width,20);
+	leftSide = new Ground(800,600,20,120);
+	rightSide = new Ground(1100,600,20,120);
+	
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(200);
+
+  ellipse(ball.position.x,ball.position.y,20); //desenhar a bola no jogo
+
+  groundObj.display();
+  leftSide.display();
+  rightSide.display();
+  
+
+  drawSprites();
+  Engine.update(engine);
+}
+
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Body.applyForce(ball,{x:0,y:0},{x:1.75,y:1.5});
+		// Body.applyForce( ball, {x: ball.position.x, y: ball.position.y}, {x: 0, y: -0.05});
+
+	}
+
+}
+
